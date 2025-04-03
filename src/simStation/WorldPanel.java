c8378dea3c056be4247aa1800bfb26776dd710d3
hunter-nguyen <hunternguyen8@gmail.com>
2025-04-02 17:34:03 -0700

    add simStation package boilerplate

---
+++ src/simStation/WorldPanel.java
@@ -0,0 +1,21 @@
+package simStation;
+
+import mvc.*;
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+public class WorldPanel extends AppPanel {
+
+    public JPanel threadPanel = new JPanel();
+
+    public WorldPanel(WorldFactory factory) {
+        // TODO: Implement this method
+    }
+
+    public void setModel(Model m) {
+        // TODO: Implement this method
+    }
+
+
+}
