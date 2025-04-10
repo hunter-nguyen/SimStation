d892c15d6a0cdf62281436bfb09e633e3383171b
snagapuri <61635758+snagapuri@users.noreply.github.com>
2025-04-09 22:51:02 -0700

    Update Cow.java

--- src/greed/Cow.java
+++ src/greed/Cow.java
@@ -1,12 +1,38 @@
 package greed;
 
-import simStation.MobileAgent;
+import simStation.*;
+import java.awt.*;
 
-public class Cow extends MobileAgent{
+public class Cow extends MobileAgent {
     private int energy = 100;
     private int greediness = 25;
 
+    @Override
     public void update(){
+        if (energy <= 0) {
+            setColor(Color.WHITE);
+            stop();
+            return;
+        }
 
+        Meadow meadow = (Meadow) getWorld();
+        Patch patch = meadow.getPatch(getX(), getY());
+
+        if (patch.getEnergy() >= greediness) {
+            patch.eatMe(this, greediness);
+        } else if (energy >= Meadow.moveEnergy) {
+            move();
+            energy -= Meadow.moveEnergy;
+        } else {
+            starve();
+        }
+    }
+
+    public void gainEnergy(int amount) {
+        energy = Math.min(100, energy + amount);
+    }
+
+    public void starve() {
+        energy -= Meadow.waitPenalty;
     }
 }
