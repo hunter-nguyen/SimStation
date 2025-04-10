a41de9ed2a61d2b946e06e193a28103f4876b5c4
coderbeansmidnight <bnvb2006@yahoo.com>
2025-04-09 19:49:43 -0700

    updated WorldFactory

--- src/simStation/WorldFactory.java
+++ src/simStation/WorldFactory.java
@@ -9,14 +9,12 @@ public class WorldFactory implements AppFactory {
 
     @Override
     public Model makeModel() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'makeModel'");
+        return new World();
     }
 
     @Override
     public View makeView(Model model) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'makeView'");
+        return new WorldView((World)w);
     }
 
     @Override
@@ -31,14 +29,19 @@ public class WorldFactory implements AppFactory {
             switch (type) {
             // cmmd = new StartCommand(model, null);
                 case "Start":
+                    cmmd = new StartCommand((World) model, (Agent) source);
                     break;
                 case "Pause":
+                    cmmd = new PauseCommand((World) model, (Agent) source);
                     break;
                 case "Resume":
+                    cmmd = new ResumeCommand((World) model, (Agent) source);
                     break;
                 case "Stop":
+                    cmmd = new StopCommand((World) model, (Agent) source);
                     break;
                 case "Stats":
+                    cmmd = new StatsCommand((World) model, (Agent) source);
                     break;
                 default:
                     break;
@@ -49,14 +52,14 @@ public class WorldFactory implements AppFactory {
 
     @Override
     public String getTitle() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTitle'");
+        return "SimStation";
     }
 
     @Override
     public String[] getHelp() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getHelp'");
+        return new String[] {"Click Start to start all agents", "Click Pause to pause all agents",
+        "Click Resume to resume all agents", "Click Stop to stop all agents",
+        "Click Stats to see statistics on agents", "click SW to move South-West", };
     }
 
     @Override
