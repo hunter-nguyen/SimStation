7b0142f2115a2888a7f0028cd9de327ac7aea055
snagapuri <61635758+snagapuri@users.noreply.github.com>
2025-04-15 20:31:10 -0700

    Update Meadow.java

--- src/greed/Meadow.java
+++ src/greed/Meadow.java
@@ -1,75 +1,41 @@
 package greed;
 
 import simStation.*;
-import java.util.*;
 
 public class Meadow extends World {
-    private int waitPenalty = 5;
-    private int moveEnergy = 10;
-    private int numCows = 50;
-    private static final int patchSize = 10;
-    private static final int dim = SIZE / patchSize;
+    public int waitPenalty = 5;
+    public int moveEnergy = 10;
+    public int numCows = 50;
+    public int dim = SIZE / Patch.patchSize;
 
     private Patch [][] patches = new Patch[dim][dim];
 
-    public Meadow(int size, int patchSize){
-        for (int i = 0; i < dim; i++) {
-            for (int j = 0; j < dim; j++) {
-                patches[i][j] = new Patch();
+    @Override
+    public void populate() {
+        // Create grid of patches
+        for (int x = 0; x < dim; x++) {
+            for (int y = 0; y < dim; y++) {
+                Patch patch = new Patch();
+                patch.setX(x * Patch.patchSize);
+                patch.setY(y * Patch.patchSize);
+                patches[x][y] = patch;
+                addAgent(patch);
             }
         }
-    }
 
-    public Patch getPatch(int xc, int yc){
-        return patches[xc/dim][yc/dim];
-    }
-
-    public int getWaitPenalty() {
-        return waitPenalty;
-    }
-
-    public void setWaitPenalty(int waitPenalty) {
-        this.waitPenalty = waitPenalty;
-    }
-
-    public int getMoveEnergy() {
-        return moveEnergy;
-    }
-
-    public void setMoveEnergy(int moveEnergy) {
-        this.moveEnergy = moveEnergy;
-    }
-
-    public int getNumCows() {
-        return numCows;
-    }
-
-    public void setNumCows(int numCows) {
-        this.numCows = numCows;
-    }
-
-    public int getDim() {
-        return dim;
-    }
-
-    public void setDim(int dim) {
-        this.dim = dim;
-    }
-
-    public void populate() {
+        // Add cows
         for (int i = 0; i < numCows; i++) {
             Cow cow = new Cow();
             addAgent(cow);
         }
     }
 
-    public void updateStatistics() {
-        super.updateStatistics();
-        for (int i = 0; i < dim; i++) {
-            for (int j = 0; j < dim; j++) {
-                patches[i][j].update();
-            }
-        }
+    public Patch getPatch(int xc, int yc) {
+        int x = xc / Patch.patchSize;
+        int y = yc / Patch.patchSize;
+        if (x >= 0 && x < dim && y >= 0 && y < dim)
+            return patches[x][y];
+        return null;
     }
 
 }
